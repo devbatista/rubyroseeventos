11042063d9c25854005c9c2be9cabba050c757c6
Rafael Batista <rafael@devbatista.com>
2023-08-09 20:56:21 -0300

    alterando esquema de cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,7 +17,7 @@ class CorsMiddleware
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'https://rubyroseeventos.com.br')
+            ->header('Access-Control-Allow-Origin', 'https://rubyroseeventos.com.br/*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
