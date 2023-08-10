5b9548c1779fe78d4f845efdf7f34ee77908416e
Rafael Batista <rafael@devbatista.com>
2023-08-09 21:57:10 -0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,23 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    // 'allowed_methods' => ['*'],
+
+    // 'allowed_origins' => ['https://rubyroseeventos.com.br'],
+
+    // 'allowed_origins_patterns' => [],
+
+    // 'allowed_headers' => ['*'],
+
+    // 'exposed_headers' => [],
+
+    // 'max_age' => 0,
+
+    // 'supports_credentials' => false,
+
+    'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
